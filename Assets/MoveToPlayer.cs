5ab7d5a2f069bd772630bd6e4b58c53989c57402
ROTHBERG <sarahrothberg@gmail.com>
2019-12-10 23:26:02 -0500

    bunch o stff

---
+++ Assets/MoveToPlayer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveToPlayer : MonoBehaviour
+{
+	public GameObject scene;
+	public GameObject player; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (Input.GetKeyDown("a"))
+		{
+			Debug.Log("realign scene");
+			Vector3 tempPos;
+			Quaternion tempRot; 
+			tempPos.x = player.transform.position.x;
+			tempPos.z = player.transform.position.z;
+			//tempRot.x = player.transform.rotation.x;
+			//tempRot.z = player.transform.rotation.z;
+			scene.transform.position = new Vector3 (tempPos.x, 0, tempPos.z);
+		//	scene.transform.rotation = new Quaternion(tempRot.x, 0, tempRot.z, 0);
+		}
+    }
+}
---
+++ Assets/Scripts/MainController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainController : MonoBehaviour
+{
+
+	public GameObject[] vignettes;
+	int currentVignetteNum;
+	public string key; 
+	
+    void Update()
+    {
+		if (Input.GetKeyDown(key)){
+			Debug.Log("keyDown");
+			NextVignette();
+		}
+        
+    }
+	
+	public void NextVignette()
+	{
+		Debug.Log("switched to" + currentVignetteNum);
+		vignettes[currentVignetteNum].SetActive(false);
+		currentVignetteNum++; 
+		if (currentVignetteNum == vignettes.Length)
+		{
+			currentVignetteNum = 0;
+		}
+		vignettes[currentVignetteNum].SetActive(true);
+	}
+}
